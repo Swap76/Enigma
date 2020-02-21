d533ee8f899784f01e900151135ee31c408cba53
Swap76 <swapnil.satish17@siesgst.ac.in>
2020-02-21 22:05:04 +0530

    Added Diffrent encryption logic for array and object

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,11 +1,20 @@
 const CryptoJS = require('crypto-js');
 
 export const encryption = async (plainText) => {
-	plainText.forEach(element => {
-		Object.entries(element).forEach(entry => {
-			let [key, value] = entry;
-			element[key] = CryptoJS.AES.encrypt(JSON.stringify(value), 'swapnil').toString();
+	if (Array.isArray(plainText)) {
+		plainText.forEach(element => {
+			Object.entries(element).forEach(entry => {
+				let [key, value] = entry;
+				element[key] = CryptoJS.AES.encrypt(JSON.stringify(value), 'swapnil').toString();
+			});
 		});
-	});
-	return plainText;
+		return plainText;
+	}
+	else {
+		Object.entries(plainText).forEach(entry => {
+			let [key, value] = entry;
+			plainText[key] = CryptoJS.AES.encrypt(JSON.stringify(value), 'swapnil').toString();
+		});
+		return plainText;
+	}
 };
