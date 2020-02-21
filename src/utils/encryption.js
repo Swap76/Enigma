bfc65f2848786712add21529168984bbf54c6e24
Swap76 <swapnil.satish17@siesgst.ac.in>
2020-02-21 23:53:58 +0530

    Problem of diffrent hashes with AES solved

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,5 +1,9 @@
 const CryptoJS = require('crypto-js');
 
+const key = 'swapnil';
+const keyutf = CryptoJS.enc.Utf8.parse(key);
+const iv = CryptoJS.enc.Base64.parse(key);
+
 export const encryption = async (plainText) => {
 	if (Array.isArray(plainText)) {
 		plainText.forEach(element => {
@@ -7,10 +11,10 @@ export const encryption = async (plainText) => {
 				let [key, value] = entry;
 				if (Array.isArray(element[key])) {
 					element[key].forEach((element,index) => {
-						element[key][index] = CryptoJS.AES.encrypt(JSON.stringify(element), 'swapnil').toString();
+						element[key][index] = CryptoJS.AES.encrypt(JSON.stringify(element), keyutf, { iv: iv }).toString();
 					});
 				} else {
-					element[key] = CryptoJS.AES.encrypt(JSON.stringify(value), 'swapnil').toString();
+					element[key] = CryptoJS.AES.encrypt(JSON.stringify(value), keyutf, { iv: iv }).toString();
 				}
 			});
 		});
@@ -22,10 +26,10 @@ export const encryption = async (plainText) => {
 			let [key, value] = entry;
 			if (Array.isArray(plainText[key])) {
 				plainText[key].forEach((element,index) => {
-					plainText[key][index] = CryptoJS.AES.encrypt(JSON.stringify(element), 'swapnil').toString();
+					plainText[key][index] = CryptoJS.AES.encrypt(JSON.stringify(element), keyutf, { iv: iv }).toString();
 				});
 			} else {
-				plainText[key] = CryptoJS.AES.encrypt(JSON.stringify(value), 'swapnil').toString();
+				plainText[key] = CryptoJS.AES.encrypt(JSON.stringify(value), keyutf, { iv: iv }).toString();
 			}
 		});
 		return plainText;
